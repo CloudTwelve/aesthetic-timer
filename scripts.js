5686f6c8cd32f6f8a946b35cfb6ed920d50cde53
CloudTwelve <94795982+CloudTwelve@users.noreply.github.com>
2025-06-21 16:31:33 +0000

    add settings lightbox toggle-ability

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-let placesToDonateTo = [
+const placesToDonateTo = [
   ["https://irusa.org/middle-east/palestine/", "Palestine"],
   ["https://www.launchgood.com/communitypage/syria_1#!/community/syria_1/", "Syria"],
   ["https://sapa-usa.org/", "Sudan"],
@@ -9,10 +9,71 @@ let placesToDonateTo = [
   ["https://www.launchgood.com/v4/campaign/palestine_mothers_and_babies?src=internal_discover", "Palestinian Mothers and Babies"]
 ];
 
+const aesthetics = [
+  ["Sleek", "Inter", ""],
+  ["Cottagecore", "Playfair Display", ""],
+  ["y2k", "LXGW Marker Gothic", ""],
+  ["Vintage", "Savate", ""],
+  ["Soft Girl", "Caveat", ""],
+  ["General", "Caprasimo", "images/General.png"]
+]
+
+const playClass = "fa-solid fa-play fa-4x";
+const pauseClass = "fa-solid fa-pause fa-4x";
+
 document.addEventListener("DOMContentLoaded", () => {
-    let donationLink = document.querySelector("#donation-link");
-    let num = Math.floor(Math.random() * placesToDonateTo.length);
-    let message = placesToDonateTo[num][1];
-    donationLink.textContent = message;
-    donationLink.href = placesToDonateTo[num][0];
+  let restartBtn = document.querySelector(".restart");
+  let settingsBtn = document.querySelector(".settings");
+  let playPauseBtn = document.querySelector(".play-pause");
+
+  let lightbox = document.querySelector(".lightbox");
+  let xBtn = document.querySelector(".x");
+  let saveTimeBtn = document.querySelector(".save-time");
+
+  let hrsInput = document.querySelector("#hrs");
+  let minsInput = document.querySelector("#mins");
+  let secsInput = document.querySelector("#secs");
+
+  let donationLink = document.querySelector("#donation-link");
+  let num = Math.floor(Math.random() * placesToDonateTo.length);
+  let message = placesToDonateTo[num][1];
+  donationLink.textContent = message;
+  donationLink.href = placesToDonateTo[num][0];
+
+  const displayLightbox = () => {
+    lightbox.style.display = "block";
+    xBtn.addEventListener("click", hideLightbox);
+    console.log("lightbox displayed!");
+    saveTimeBtn.addEventListener("click", handleTimerChange);
+  }
+
+  const hideLightbox = () => {
+    xBtn.removeEventListener("click", hideLightbox);
+    lightbox.style.display = "none";
+  }
+
+  const handleTimerChange = () => {
+    let hrs = Number(hrsInput.value);
+    let mins = Number(minsInput.value);
+    let secs = Number(secsInput.value);
+
+    let minsIncorrect = Number(minsInput.value) >= 60;
+    let secsIncorrect = Number(secsInput.value) >= 60;
+
+    if (minsIncorrect || secsIncorrect) {
+      alert("Minutes/seconds must not be greater than 60.");
+      return -1;
+    }
+
+    localStorage.setItem("hours", hrs);
+    localStorage.setItem("minutes", mins);
+    localStorage.setItem("seconds", secs);
+
+    hrsInput.value = "";
+    minsInput.value = "";
+    secsInput.value = "";  
+  }
+
+  settingsBtn.addEventListener("click", displayLightbox);
+
 });
